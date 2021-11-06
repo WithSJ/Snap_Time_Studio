9a21fdc724b0315a1d2e85760c8f0462dbdda5f1
Sandeep Jadam <withsj.in@gmail.com>
2021-11-06 16:11:41 +0530

    git update

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,15 @@ from app import app
 @app.route("/photography")
 def photography():
     return render_template("photography.html", title="Photography")
+
+@app.route("/videography")
+def videography():
+    return render_template("videography.html", title="Videography")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+@app.route("/book_a_session")
+def book_a_session():
+    return render_template("book_a_session.html", title="Book A Session")
