5068b8b4e98e4dbfea1b8511facfae0bd3157441
Sandeep Jadam <withsj.in@gmail.com>
2022-03-05 22:03:24 +0530

    admin pannel upgrade issue #1 solved

--- app/routes.py
+++ app/routes.py
@@ -28,76 +28,6 @@ def videography():
 def about():
     return render_template("about.html", title="About")
 
-@app.route("/admin",methods=['GET', 'POST'])
-def admin():
-    form = dict()
-    form["photoUploadForm"]=PhotoUploadForm()
-    form["videoAddForm"]= VideoUploadForm()
-    adminTab = {"TabNumber":1}
-    
-    # if form["photoUploadForm"].submit.data == True:
-    #     photo = form["photoUploadForm"].photo.data
-    #     photoTitle = form["photoUploadForm"].title.data
-        
-    #     # GEt Date
-    #     if form["photoUploadForm"].autodate.data == True:
-    #         dateTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    #     else:
-    #         dateTime = str(
-    #             form["photoUploadForm"].year.data) + "-"+ str(
-    #             form["photoUploadForm"].month.data) +"-"+ str(
-    #             form["photoUploadForm"].day.data)
-    #     # End Geting Date
-        
-        
-    #     # dataSet = {
-    #     #     "photoTitle":photoTitle,
-    #     #         "photo":photo,
-    #     #     "dateTime":dateTime,}
-        
-    #     post_new_photo(
-    #         id= str(time.time()).replace(".",""),
-    #         title= photoTitle,dateTime=dateTime,image=photo)
-        
-    #     allPhotos = get_all_photos()
-    #     adminTab = {"TabNumber":3}
-    #     return render_template(
-    #         "admin_pannel.html", title="Admin",form=form,adminTab=adminTab,
-    #         allPhotos = allPhotos
-    #     )
-
-    
-    if form["videoAddForm"].submit.data == True:
-        videoTitle = form["videoAddForm"].title.data
-        videoUrl = form["videoAddForm"].videoUrl.data
-
-        # dataSet = {
-        #     "videoTitle":videoTitle,
-        #     "videoUrl" : videoUrl
-        # }
-        dateTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        post_new_video(
-            id= str(time.time()).replace(".",""),
-            title=videoTitle,
-            dateTime=dateTime,
-            videoUrl=videoUrl)
-
-        allVideos = get_all_videos()
-        adminTab = {"TabNumber":4}
-
-        return render_template(
-            "admin_pannel.html", title="Admin",form=form,adminTab=adminTab,
-            allVideos = allVideos)
-
-
-    allPhotos = get_all_photos()
-    allVideos = get_all_videos()
-
-    return render_template(
-        "admin_pannel.html", title="Admin",form=form,adminTab=adminTab,
-        allPhotos=allPhotos,allVideos=allVideos
-    )
-
 @app.route("/book_a_session")
 def book_a_session():
     return render_template("book_a_session.html", title="Book A Session")
@@ -152,3 +82,66 @@ def login():
 
 
     return render_template("login.html",title="Log in",form=form,forgot_form=forgot_form)
+
+
+
+
+
+@app.route("/admin/dashboard")
+@app.route("/admin")
+def admin_dashboard():
+    
+    return render_template("admin_pannel_dashboard.html", title="Admin")
+
+
+
+@app.route("/admin/photos",methods=['GET', 'POST'])
+def admin_photos():
+    form=PhotoUploadForm()
+    
+    if form.submit.data == True:
+        photo = form.photo.data
+        photoTitle = form.title.data
+        
+        # GEt Date
+        if form.autodate.data == True:
+            dateTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            dateTime = str(
+                form.year.data) + "-"+ str(
+                form.month.data) +"-"+ str(
+                form.day.data)
+        # End Geting Date
+        
+        post_new_photo(
+            id= str(time.time()).replace(".",""),
+            title= photoTitle,dateTime=dateTime,image=photo)
+
+    allPhotos = get_all_photos()
+
+    return render_template("admin_pannel_photos.html", title="Admin",
+                            form=form,allPhotos=allPhotos)
+
+
+
+@app.route("/admin/videos",methods=['GET', 'POST'])
+def admin_videos():
+    
+    form= VideoUploadForm()
+        
+    if form.submit.data == True:
+        videoTitle = form.title.data
+        videoUrl = form.videoUrl.data
+
+        dateTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        post_new_video(
+            id= str(time.time()).replace(".",""),
+            title=videoTitle,
+            dateTime=dateTime,
+            videoUrl=videoUrl)
+
+    allVideos = get_all_videos()
+
+    return render_template(
+        "admin_pannel_videos.html", title="Admin",form=form,allVideos=allVideos
+    )
